dd92cfad3669d2a27d269826833b5e581ccaa3f0
ntasti <ntaasti@gmail.com>
2025-11-15 01:38:06 +0400

    обновленный UI для главной страницы

--- src/main/java/org/familybudget/familybudget/HelloApplication.java
+++ src/main/java/org/familybudget/familybudget/HelloApplication.java
@@ -12,7 +12,7 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 1000, 800);
+        Scene scene = new Scene(fxmlLoader.load(), 800, 600);
 
 //        // Подключение BootstrapFX :
         scene.getStylesheets().add(BootstrapFX.bootstrapFXStylesheet());
